400e3dc58d5b8bddec68daf9c083f7b861ba50bc
kingveee <vivianpham22@gmail.com>
2017-07-11 10:06:43 -0400

    build cat class

--- IntroOOP/Cat.cs
+++ IntroOOP/Cat.cs
@@ -13,5 +13,48 @@ namespace IntroOOP
         // They should always be names in Pascal case.
 
 
+        // In OOP are 2 types. 
+        // - Fields: a variable that belongs to a class
+        private string name;
+        private int age;
+        private string furColor;
+        private bool isHungry;
+
+        // - Properties: a sort of mix between a field and a method that allow controlled access
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+
+        // We also have behavior.
+        // - Methods: repeatable/reusable sections of code - Actions
+        // - Constructors: specialized methods that are used from instantiating an object
+        //         - Allows us to control the sata of an object upon creation.
+        //         - Allows us to perform any action the object might need upon creation/
+        //         - Always shares the name of the class
+        public Cat()
+        {
+            // This is an example of a default constructor. A default constructor takes
+            // no arugments/paramaters.
+        }
+
+        public Cat(string name, int age, string furColor)
+        {
+            this.name = name;
+            this.age = age;
+            this.furColor = furColor;
+        }
+        //This Cat has a method.
+        public void Eat()
+        {
+            if (isHungry)
+            {
+                isHungry = false;
+            }
+            Console.WriteLine("Is the cat hungry?" + isHungry);
+        }
+
     }
 }
